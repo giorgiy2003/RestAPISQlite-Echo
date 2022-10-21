a3fb14d45d740ca26df6db5f74be567298071e68
Giorgiy Baroyan <giorgiy_03@mail.ru>
2022-10-21 18:32:06 +0300

    Рефакторинг кода

--- RestAPIwithSQLITE3/internal/handlers/Person_handlers.go
+++ RestAPIwithSQLITE3/internal/handlers/Person_handlers.go
@@ -11,35 +11,35 @@ import (
 )
 
 func Add(c echo.Context) error {
-	var p model.Person
-	p.Email = c.FormValue("email")
-	p.Phone = c.FormValue("phone")
-	p.FirstName = c.FormValue("firstName")
-	p.LastName = c.FormValue("lastName")
-	err := logic.Create(p)
+	var newPerson model.Person
+	newPerson.Email = c.FormValue("email")
+	newPerson.Phone = c.FormValue("phone")
+	newPerson.FirstName = c.FormValue("firstName")
+	newPerson.LastName = c.FormValue("lastName")
+	err := logic.Create(newPerson)
 	if err != nil {
 		log.Println(err)
-		return c.JSON(http.StatusBadRequest, err)
+		return c.JSON(http.StatusBadRequest, fmt.Sprint(err))
 	}
-	log.Println("Создана запись:", p)
-	return c.JSON(http.StatusCreated, p)
+	log.Println("Добавлена запись", newPerson)
+	return c.JSON(http.StatusCreated, newPerson)
 }
 
 func GetAll(c echo.Context) error {
-	persons, err := logic.GetAll()
+	persons, err := logic.Read()
 	if err != nil {
 		log.Println(err)
-		return c.JSON(http.StatusBadRequest, err)
+		return c.JSON(http.StatusBadRequest, fmt.Sprint(err))
 	}
 	return c.JSON(http.StatusOK, persons)
 }
 
 func GetById(c echo.Context) error {
 	id := c.Param("id")
-	persons, err := logic.GetById(id)
+	persons, err := logic.ReadOne(id)
 	if err != nil {
 		log.Println(err)
-		return c.JSON(http.StatusBadRequest, err)
+		return c.JSON(http.StatusBadRequest, fmt.Sprint(err))
 	}
 	log.Println(persons)
 	return c.JSON(http.StatusOK, persons)
@@ -50,7 +50,7 @@ func Delete(c echo.Context) error {
 	err := logic.Delete(id)
 	if err != nil {
 		log.Println(err)
-		return c.JSON(http.StatusBadRequest, err)
+		return c.JSON(http.StatusBadRequest, fmt.Sprint(err))
 	}
 	log.Printf("Запись с id = %s  успешно удалена", id)
 	return c.JSON(http.StatusOK, fmt.Sprintf("Запись с id = %s  успешно удалена", id))
@@ -66,7 +66,7 @@ func Update(c echo.Context) error {
 	err := logic.Update(newPerson, id)
 	if err != nil {
 		log.Println(err)
-		return c.JSON(http.StatusBadRequest, err)
+		return c.JSON(http.StatusBadRequest, fmt.Sprint(err))
 	}
 	log.Printf("Запись с id = %s  успешно обновлена", id)
 	return c.JSON(http.StatusOK, fmt.Sprintf("Запись с id = %s  успешно обновлена", id))
--- RestAPIwithSQLITE3/internal/logic/Person_logic.go
+++ RestAPIwithSQLITE3/internal/logic/Person_logic.go
@@ -1,50 +1,12 @@
 package logic
 
 import (
+	"errors"
+	"fmt"
 	model "myapp/internal/model"
 	repository "myapp/internal/repository"
 	"strconv"
 )
-
-func GetAll() ([]model.Person, error) {
-	rows, err := repository.Connection.Query(`SELECT * FROM "person" ORDER BY "person_id"`)
-	if err != nil{
-	   return nil, err
-   }
-   defer rows.Close()
-   persons := []model.Person{}
-   for rows.Next(){
-   var p model.Person
-	   err := rows.Scan(&p.Id,&p.Email,&p.Phone,&p.FirstName, &p.LastName)
-	   if err != nil{
-		   return nil,err
-	   }
-	   persons = append(persons, p)
-   }
-   return persons, nil
-}
-
-func GetById(id string) ([]model.Person, error) {
-	person_id, err := strconv.Atoi(id)
-	if err != nil {
-		return nil, err
-	}
-	rows, err := repository.Connection.Query(`SELECT * FROM "person" WHERE "person_id" = $1`, person_id)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-	persons := []model.Person{}
-	for rows.Next(){
-	var p model.Person
-		err := rows.Scan(&p.Id,&p.Email,&p.Phone,&p.FirstName, &p.LastName)
-		if err != nil{
-			return nil,err
-		}
-		persons = append(persons, p)
-	}
-	return persons, nil
-}
 
 func Create(p model.Person) error {
 	if _, err := repository.Connection.Exec(`INSERT INTO "person" ("person_email", "person_phone", "person_firstName", "person_lastName") VALUES ($1, $2,$3,$4)`, p.Email, p.Phone, p.FirstName, p.LastName); err != nil {
@@ -53,26 +15,71 @@ func Create(p model.Person) error {
 	return nil
 }
 
+func ReadOne(id string) ([]model.Person, error) {
+	person_id, err := strconv.Atoi(id)
+	if err != nil {
+		return nil, errors.New("Error: неверно введён параметр id")
+	}
+	row, err := repository.Connection.Query(`SELECT * FROM "person" WHERE "person_id" = $1`, person_id)
+	if err != nil {
+		return nil, err
+	}
+	var personInfo = []model.Person{}
+	for row.Next() {
+		var p model.Person
+		err := row.Scan(&p.Id, &p.Email, &p.Phone, &p.FirstName, &p.LastName)
+		if err != nil {
+			return nil, err
+		}
+		personInfo = append(personInfo, p)
+	}
+	return personInfo, nil
+}
+
+func Read() ([]model.Person, error) {
+	row, err := repository.Connection.Query(`SELECT * FROM "person" ORDER BY "person_id"`)
+	if err != nil {
+		return nil, err
+	}
+	var personInfo = []model.Person{}
+	for row.Next() {
+		var p model.Person
+		err := row.Scan(&p.Id, &p.Email, &p.Phone, &p.FirstName, &p.LastName)
+		if err != nil {
+			return nil, err
+		}
+		personInfo = append(personInfo, p)
+	}
+	return personInfo, nil
+}
+
 func Update(p model.Person, id string) error {
-	person_id,err := strconv.Atoi(id)
-	if err != nil{
+	if err := dataExist(id); err != nil {
 		return err
 	}
-	if _, err := repository.Connection.Exec(`UPDATE "person" SET "person_email" = $1,"person_phone" = $2,"person_firstName" = $3,"person_lastName" = $4  WHERE "person_id" = $5`, p.Email, p.Phone, p.FirstName, p.LastName, person_id); err != nil {
+	if _, err := repository.Connection.Exec(`UPDATE "person" SET "person_email" = $1,"person_phone" = $2,"person_firstName" = $3,"person_lastName" = $4  WHERE "person_id" = $5`, p.Email, p.Phone, p.FirstName, p.LastName, id); err != nil {
 		return err
 	}
 	return nil
 }
 
 func Delete(id string) error {
-	person_id,err := strconv.Atoi(id)
-	if err != nil{
+	if err := dataExist(id); err != nil {
 		return err
 	}
-	if _, err := repository.Connection.Exec(`DELETE FROM "person" WHERE "person_id" = $1`, person_id); err != nil {
+	if _, err := repository.Connection.Exec(`DELETE FROM "person" WHERE "person_id" = $1`, id); err != nil {
 		return err
 	}
 	return nil
 }
 
-
+func dataExist(id string) error {
+	persons, err := ReadOne(id)
+	if err != nil {
+		return err
+	}
+	if len(persons) == 0 {
+		return fmt.Errorf("Error: записи с id = %s не существует", id)
+	}
+	return nil
+}
